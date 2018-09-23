4b9ea1e97ecd331af80bad1172b728be23aa534d
Zak652 <m2kzak@gmail.com>
2018-09-23 07:35:28 +0300

    First production environment changes to test setup

--- arm/db_manage.py
+++
@@ -1,9 +0,0 @@
-from flask_migrate import Migrate, MigrateCommand
-from arm.database import Base
-
-class DB(object):
-    def __init__(self, metadata):
-        self.metadata = metadata
-
-migrate = Migrate(app, DB(Base.metadata))
-manager.add_command('db', MigrateCommand)
